f3ee7250ee89b22eb50223254f5dc7c4469c1b04
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-21 22:49:41 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,7 +1,7 @@
 class Admin::PreferencesController < ActionController::Base
 	def index
-    	@preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
-  	end
+    @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
+  end
 
 	def update
 		@preference = Preference.find(params[:id])
