54d777d2a1996774c5c1325f8247c3d0b225541d
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-21 22:52:02 +0000

    Automatically backed up by Learn

--- db/migrate/20201021183000_create_preferences.rb
+++ db/migrate/20201021183000_create_preferences.rb
@@ -1,10 +1,12 @@
-class Preferences < ActiveRecord::Migration
+class CreatePreferences < ActiveRecord::Migration
   def change
-  	create_table :preferences do |t|
-      t.string :artist_sort_order
-      t.string :song_sort_order
-      t.boolean :allow_create_songs
-      t.boolean :allow_create_artists
-  	end
+    create_table :preferences do |t|
+      t.string :song_sort_order, default: "DESC"
+      t.string :artist_sort_order, default: "DESC"
+      t.boolean :allow_create_artists, default: true
+      t.boolean :allow_create_songs, default: true
+
+      t.timestamps null: false
+    end
   end
 end
