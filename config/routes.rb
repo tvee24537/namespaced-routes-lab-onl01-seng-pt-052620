3efedfa3f1f7b584b67fe7f447be258735397852
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-21 22:48:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :admin do 
-    resources :preferences, only: [:index]
+  namespace :admin do
+  	resources :preferences, only: [:index, :update]
   end
 
   resources :artists do
